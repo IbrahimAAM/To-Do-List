a9f19ac0c3eb66a766eb60eb37b39109c4f04afa
IbrahimAAM <139483296+IbrahimAAM@users.noreply.github.com>
2025-07-01 16:10:02 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,65 @@
- const list = document.querySelector('ul');
-      const input = document.querySelector('input');
-      const button = document.querySelector('button');
+const ITEMS_CONTAINER = document.getElementById("items");
+const ITEMS_TEMPLATE = document.getElementById("itemTemplate");
+const ADD_BUTTON = document.getElementById("add");
 
-      button.addEventListener('click', () => {
-        const myItem = input.value;
-        input.value = '';
+let items = getItems();
 
-        const listItem = document.createElement('li');
-        const listText = document.createElement('span');
-        const listBtn = document.createElement('button');
+function getItems() {
+    const value = localStorage.getItem("items") || "[]";
+    return JSON.parse(value);
+}
 
-        listItem.appendChild(listText);
-        listText.textContent = myItem;
-        listItem.appendChild(listBtn);
-        listBtn.textContent = 'Complete';
-        list.appendChild(listItem);
+function setItems(items) {
+    const itemsJson = JSON.stringify(items);
+    localStorage.setItem("items", itemsJson);
+}
 
-        listBtn.addEventListener('click', () => {
-          list.removeChild(listItem);
+function addItem() {
+    items.unshift({
+        description: "",
+        completed: false
+    });
+    setItems(items);
+    refreshList();
+}
+
+function updateItem(item, key, value) {
+    item[key] = value;
+    setItems(items);
+    refreshList();
+}
+
+function refreshList() {
+    ITEMS_CONTAINER.innerHTML = "";
+
+    for (const [index, item] of items.entries()) {
+        const itemElement = ITEMS_TEMPLATE.content.cloneNode(true);
+        const descriptionInput = itemElement.querySelector(".item-description");
+        const completedInput = itemElement.querySelector(".item-completed");
+        const deleteBtn = itemElement.querySelector(".delete-btn");
+
+        descriptionInput.value = item.description;
+        completedInput.checked = item.completed;
+
+        descriptionInput.addEventListener("change", () => {
+            updateItem(item, "description", descriptionInput.value);
+        });
+        completedInput.addEventListener("change", () => {
+            updateItem(item, "completed", completedInput.checked);
+        });
+        
+        deleteBtn.addEventListener("click", () => {
+            items.splice(index, 1);
+            setItems(items);
+            refreshList();
         });
 
-        input.focus();
-      });
+        ITEMS_CONTAINER.append(itemElement);
+    }
+}
+
+ADD_BUTTON.addEventListener("click", () => {
+    addItem();
+});
+
+refreshList();
